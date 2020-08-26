1276d4d2313fb7ca5fec5fca6a57cba14aca2eda
dengdeng-zhong <862283389@qq.com>
2020-08-27 00:00:27 +0800

    websocket的引入
    Signed-off-by: dengdeng-zhong <862283389@qq.com>

--- app.js
+++ app.js
@@ -15,3 +15,19 @@ const respDataMiddleware = require('./middleware/koa_response_data')
 app.use(respDataMiddleware)
 //3.绑定端口 30000
 app.listen(8081)
+
+const WebSocket = require('ws')
+// 创建WebSocket服务端的对象,绑定的端口号是8084
+const wss = new WebSocket.Server({
+  port: 8084
+})
+// 对客户端的连接事件进行监听
+wss.on('connection', client => {
+  console.log('有一个客户端连接进来了..')
+  // 对客户端的连接对象进行message事件的监听
+  // msg: 由客户端发给服务器的数据
+  client.on('message', msg => {
+    console.log('客户端发送数据给服务端的了: '+msg)
+    client.send('hello socket from Server')
+  })
+})
